ee61e1cece283b62be9bca1bcec7d7021b41e6f3
= <=>
2022-07-18 13:49:17 -0400

    version 1

--- sample/sample.py
+++ sample/sample.py
@@ -1,1 +1,12 @@
+#this is a sample of functions to use
 
+def print_hi():
+    print('HI!')
+    print('*'*10)
+
+def print_list(x0, y0)
+    print('Start at', x0)
+    print('end at', y0)
+    l = range(x0, y0)
+    print(l)
+    return l
