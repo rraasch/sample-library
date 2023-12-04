c9392350cfb11ad76f9db8e456f26e112de41ee8
Ryan Raasch <ryanraasch94@gmail.com>
2023-12-04 07:58:42 -0800

    Update sample.py

--- sample/sample.py
+++ sample/sample.py
@@ -1,4 +1,5 @@
 #this is a sample of functions to use
+import os
 
 def print_hi():
     print('HI!')
@@ -10,3 +11,6 @@ def print_list(x0, y0):
     l = range(x0, y0)
     print(l)
     return l
+
+def print_wd():
+    print(os.getcwd())
